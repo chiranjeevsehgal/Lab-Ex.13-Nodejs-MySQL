a61c526f78cacbfdaeed7e455e2f514372ab51a7
chiranjeevsehgal <chiranjeevsehgal@gmail.com>
2023-10-12 19:13:04 +0530

    Minor changes

--- server.js
+++ server.js
@@ -1,28 +1,49 @@
-var mysql = require('mysql');
+const express = require("express");
+const mysql = require('mysql');
+
 require('dotenv').config();
-var connection = mysql.createConnection({
+
+const db = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
-connection.connect();
+const app = express();
 
-connection.query(
-  'create table crud_stud(id int,name varchar(255));',
-  function (error, results, fields) {
-    if (error) throw error;
-    console.log(results);
+
+db.connect((err) => {
+  if (err) {
+    console.error("There is a Database connection error:", err);
+  } else {
+    console.log("Connected to MySQL database sucessfuly!");
   }
-);
+});
 
-// connection.query(
-//   'select * from crud_student;',
-//   function (error, results, fields) {
-//     if (error) throw error;
-//     console.log(results);
-//   }
-// );
+// Example route for searching available rooms
+app.get("/search-rooms", (req, res) => {
+  const { checkInDate, checkOutDate, roomType, occupancy } = req.query;
 
-connection.end();
+  const query = `SELECT * FROM rooms`;
+
+  const values = [roomType, occupancy, checkInDate, checkOutDate];
+
+  // Execute the query
+  db.query(query, values, (error, results) => {
+    if (error) {
+      console.error("Error executing MySQL query:", error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while fetching rooms." });
+    } else {
+      // Send the results back to the client
+      res.json(results);
+    }
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
